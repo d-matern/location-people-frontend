ba6b1f9026be84fe23e3acf3aea313df05590a57
Denis Matern <denismatern@gmail.com>
2025-02-09 21:26:59 +0300

    feature: user geolocation

---
+++ src/components/NearbyUsers.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useLocationStore } from '@/stores/location/store';
+
+const locationStore = useLocationStore();
+</script>
+
+<template>
+  <div>
+    <h3>Ближайшие пользователи в радиусе 5км:</h3>
+
+    <ul>
+      <li v-for="user in locationStore.nearbyUsers" :key="user.id">
+        ID: {{ user.id }} ({{ user.lat.toFixed(4) }}, {{ user.lng.toFixed(4) }})
+      </li>
+    </ul>
+  </div>
+</template>
